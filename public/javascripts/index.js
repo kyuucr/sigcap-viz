6d98c0192e0544691f6c97a5763f69ab466ec860
Muhammad Iqbal CR <muh.iqbal.cr@gmail.com>
2024-09-20 17:51:09 -0400

    Implement GPS coordinate picker.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -148,13 +148,239 @@ function fetchFiles(mode) {
 
 const mapModal = new bootstrap.Modal(document.getElementById("mapModal"))
 
-function map() {
+function showMap() {
   if (selectedFiles.length === 0) {
     window.alert("Please select at least 1 file !")
     return
   }
 
   mapModal.show()
+}
+
+function selectOnMap() {
+  mapModal.show()
+}
+
+const plainMap = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#f5f5f5"
+    }
+    ]
+  },
+  {
+    "elementType": "labels",
+    "stylers": [
+    {
+      "visibility": "on"
+    }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#616161"
+    }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+    {
+      "color": "#f5f5f5"
+    }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#bdbdbd"
+    }
+    ]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "stylers": [
+    {
+      "visibility": "off"
+    }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#eeeeee"
+    }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#757575"
+    }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#e5e5e5"
+    }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#9e9e9e"
+    }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#ffffff"
+    }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#757575"
+    }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#dadada"
+    }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#616161"
+    }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#9e9e9e"
+    }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#e5e5e5"
+    }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#eeeeee"
+    }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+    {
+      "color": "#c9c9c9"
+    }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+    {
+      "color": "#9e9e9e"
+    }
+    ]
+  }
+]
+
+let currentRectangle = null; // Variable to store the current rectangle
+
+function initMap() {
+  // let chiCenter = { lat: 41.877504, lng: -87.631403 }
+  let ndCenter = { lat: 41.69849558373332, lng: -86.23473923357669 }
+  map = new google.maps.Map(document.getElementById("heatmap"), {zoom: 12, center: ndCenter});
+  // let transitLayer = new google.maps.TransitLayer();
+  // transitLayer.setMap(map);
+  map.set('styles', plainMap);
+
+  // infoWindow = new google.maps.InfoWindow();
+
+  const drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.RECTANGLE, // Set drawing mode to rectangle
+    drawingControl: true, // Show drawing controls on the map
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [google.maps.drawing.OverlayType.RECTANGLE]
+      // Limit to rectangle
+    }
+  });
+  drawingManager.setMap(map);
+
+  google.maps.event.addListener(drawingManager, 'rectanglecomplete', function(rectangle) {
+    // Remove the previous rectangle if it exists
+    if (currentRectangle) {
+      currentRectangle.setMap(null);
+    }
+
+    currentRectangle = rectangle; // Update the current rectangle
+
+    const bounds = rectangle.getBounds();
+    const sw = bounds.getSouthWest();
+    const ne = bounds.getNorthEast();
+
+    console.log('Bounding Coordinates:');
+    console.log('Southwest:', sw.lat(), sw.lng());
+    document.getElementById("inputGpsStart").value = `${sw.lat()}, ${sw.lng()}`
+    document.getElementById("previewGpsStart").value = `${sw.lat()}, ${sw.lng()}`
+    console.log('Northeast:', ne.lat(), ne.lng());
+    document.getElementById("inputGpsEnd").value = `${ne.lat()}, ${ne.lng()}`
+    document.getElementById("previewGpsEnd").value = `${ne.lat()}, ${ne.lng()}`
+  });
 }
 
 fetchFileList()
